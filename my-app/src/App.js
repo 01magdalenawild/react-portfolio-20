d1e65b95ae0eba32648fbed0baf9a451e804eaa5
Magdalena Wild <maggieprophete2425@gmail.com>
2023-04-17 21:48:03 -0400

    adding work area

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
+
 function App() {
   return (
     <div >
@@ -9,6 +11,7 @@ function App() {
       <Home />
       <About />  
       <Skills />
+      <Work />
     </div>
   );
 }

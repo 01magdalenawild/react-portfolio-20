b5ad6058ea9ebed24c34e08082424c18cecd5e2f
Magdalena Wild <maggieprophete2425@gmail.com>
2023-04-20 18:14:57 -0400

    fixing files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+import { FaBars, } from 'react-icons/fa'
+const Navbar = () => {
+    const [nav, setNav]= useState(false)
+    const handleClick = () => setNav(!nav)
+    
+    return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0f1014] text-gray-300'>
+    
+
+
+    {/*menu*/}
+<ul className='hidden md:flex'>
+    <li>Home</li>
+    <li>About</li>
+    <li>Skills</li>
+    <li>Projects</li>
+    <li>Contact</li>
+   
+</ul>
+
+
+{/*Hamburger*/}
+<div onClick={handleClick} className='md:hidden z-10'>
+<FaBars />
+</div>
+
+{/*mobile menu*/}
+<ul className={!nav ? 'hidden' : 'absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items center'}>
+<li>Home</li>
+    <li className='py-6 text-4xl '>About</li>
+    <li className='py-6 text-4xl'>Skills</li>
+    <li className='py-6 text-4xl'>Projects</li>
+    <li className='py-6 text-4xl'>Contact</li>
+   
+</ul>
+       
+       
+       
+       {/*social icons*/}
+       <div className='hidden'>
+
+       </div>
+</div>
+    )
+}
+
+export default Navbar
---
+++ src/components/Work.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import Notetaker from '../assets/projects/Notetaker.png';
+import Password from '../assets/projects/Password.png'
+import Quiz from '../assets/projects/Quiz.png'
+import Project1 from '../assets/projects/Project1.png'
+import Project2 from '../assets/projects/Project2.png'
+
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#4c2a50] '>
+<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div className='pb-8'>
+        <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Work</p>
+        <p className='py-6'>// Check out some of my work</p>
+                  
+    </div>
+
+<div style={{backgroundImage:'url()'}}
+className='grid sm:grid-cols-6 md:grid-cols-3 gap-4'>
+    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content div'>
+    <div>
+        <div class="h-70 w-70"></div>
+  <img src={ Notetaker } alt="notetaker Image" />
+  <p>Notetaker</p>
+  <a href="links" />
+</div>
+
+<div>
+  <img src={ Password } alt="password Image" />
+  <p>Password Generator</p>
+  <a href="links" />
+</div>
+<div>
+  <img src={ Quiz} alt="Quiz Image" />
+  <p>Quiz </p>
+  <a href="links" />
+</div>
+<div>
+  <img src={ Project1 } alt="project Image" />
+  <p>Project 1</p>
+  <a href="links" />
+</div>
+<div>
+  <img src={ Project2 } alt="project Image" />
+  <p>Project 2</p>
+  <a href="links" />
+</div>
+       <div>
+            <span>
+
+            </span>
+            <div className='pt-8 text-center'>
+                
+                <a href="/">
+                    <button></button>
+                </a>
+                <a href="/">
+                    <button></button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+    </div>
+  )
+}
+
+export default Work
